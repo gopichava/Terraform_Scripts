83b1ced8c6e630c71e081105f851827fca7c4653
gopidevops321 <43982616+gopichava@users.noreply.github.com>
2020-06-06 07:44:55 +0530

    Update init_script.sh

--- VPC/init_script.sh
+++ VPC/init_script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install wget unzip -y
+sudo yum install httpd -y
+sudo service httpd start
+mkdir -p /var/www/html
+echo "myserver" > /var/www/html/index.html
